8eee2a01c8c8d4edc8cf98d8e99dcd7843f16deb
subu53 <subusam5@gmail.com>
2026-03-15 02:53:07 +0300

    update model.py
    update

--- app/model.py
+++ app/model.py
@@ -1,7 +1,11 @@
+from pathlib import Path
 import json
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_DIR = BASE_DIR / "models"
 
 def get_transforms():
     return transforms.Compose([
@@ -18,11 +22,14 @@ def load_model():
     in_features = model.classifier[1].in_features
     model.classifier[1] = nn.Linear(in_features, 3)
 
-    model.load_state_dict(torch.load("models/efficientnet_lung_v1.pth", map_location=device))
+    model_path = MODEL_DIR / "efficientnet_lung_v1.pth"
+    class_path = MODEL_DIR / "class_names.json"
+
+    model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     model.eval()
 
-    with open("models/class_names.json", "r") as f:
+    with open(class_path, "r") as f:
         class_names = json.load(f)
 
     return model, class_names, device
